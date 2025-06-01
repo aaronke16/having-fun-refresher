041d71f6bd8f7604a3bbdc5d8a5579c9fdd8fb0a
aaronke16 <124176003+aaronke16@users.noreply.github.com>
2025-06-01 17:28:51 -0400

    Revert "Cleaning up app.java"
    This reverts commit a389dbc7ab74b907932a0f2e2d5585504e4b2296.

--- demo/src/main/java/com/universitylib/App.java
+++ demo/src/main/java/com/universitylib/App.java
@@ -2,7 +2,35 @@ package com.universitylib;
 
 public class App {
     public static void main(String[] args) {
-        
+        Course course = new Course(0, 3);
+        Student student = new Student(10, 3.2, 10);
+        Student student2 = new Student(10, 3.2, 2);
+
+        int code = course.getCode();
+        System.out.println(code);
+
+        course.addCourse(code);
+
+        student.enrollStudent(course);
+        student2.enrollStudent(course);
+
+        course.viewCourseEnrollment(code);
+
+        student2.dropStudent(course);        
+
+        course.viewCourseEnrollment(code);
+
+        course.getWaitlist(code);
+
+        course.addToWaitlist(student, code);
+
+        course.getWaitlist(code);
+
+        course.removeFromWaitlist(student);
+
+        course.getWaitlist(code);
+
+
     }
 
     public class CouseScenarios {
